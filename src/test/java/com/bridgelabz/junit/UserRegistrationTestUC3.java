8b4eb6df0cbfc6443a7fcd9b01931456cf0af04d
Ameet Jadhav <ammugs11@gmail.com>
2022-09-30 20:12:15 +0530

    added enter valid email EX-abc.xyz@bl.co.in

---
+++ src/test/java/com/bridgelabz/junit/UserRegistrationTestUC3.java
@@ -0,0 +1,48 @@
+package com.bridgelabz.junit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridglabz.junit.UserRegistrationUC3;
+
+public class UserRegistrationTestUC3 {
+	
+	UserRegistrationUC3 userRegistration = new UserRegistrationUC3();
+
+
+	@Test
+	public void givenFirstName_WhenInFormat_ShouldReturnTrue() {
+		boolean result = userRegistration.firstName("Ameeth");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenFirstName_WhenOurOfFormat_ShouldReturnFlase() {
+		boolean result = userRegistration.firstName("ameeth");
+		Assert.assertEquals(false, result);
+	}
+
+	@Test
+	public void givenLastName_WhenInFormat_ShouldReturnTrue() {
+		boolean result = userRegistration.lastName("Ameet");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenLastName_WhenOurOfFormat_ShouldReturnFalse() {
+		boolean result = userRegistration.lastName("ameet");
+		Assert.assertEquals(false, result);
+	}
+	@Test
+	public void givenEmail_WhenInFormat_ShouldReturnTrue() {
+		boolean result = userRegistration.emailAddress("Ameetc@bl.co.in");
+		Assert.assertEquals(false, result);
+	}
+
+	@Test
+	public void givenEmail_WhenOurOfFormat_ShouldReturnFalse() {
+		boolean result = userRegistration.emailAddress("abc()*@gmail.com");
+		Assert.assertEquals(false, result);
+	}
+
+}
